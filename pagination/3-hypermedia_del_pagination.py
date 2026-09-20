0f1c6e548bf66510949bc9c1a08905fdebfcafcc
muvunyiduke03 <d.ndamagem@alustudent.com>
2026-09-20 09:31:36 +0000

    Hypermedia deletion

--- pagination/3-hypermedia_del_pagination.py
+++ pagination/3-hypermedia_del_pagination.py
@@ -52,8 +52,7 @@ class Server:
             }
         return self.__indexed_dataset
 
-    def get_hyper_index(self, index: int = None,
-                         page_size: int = 10) -> Dict:
+    def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
         """Return a deletion-resilient page of the indexed dataset.
 
         Args:
